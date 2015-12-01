4acca117f5f8bc01c287f4c0d948fc01011ca889
Constantin Titarenko <constantin.titarenko@gmail.com>
2015-12-01 18:45:10 +0200

    Set ansi as default encoding for file appender

--- appenders/file.js
+++ appenders/file.js
@@ -12,6 +12,9 @@ var EOL = require('os').EOL;
 
 module.exports = {
 	name: 'file',
+	defaults: {
+		encoding: 'ansi',
+	},
 	build: build
 };
 
@@ -23,26 +26,26 @@ function build (config) {
 		mkdirpSync(path.dirname(staticFilename));
 	}
 
-	var write = staticFilename && writeStaticFilename || (config.mkdirp 
+	var write = staticFilename && _.partial(append, staticFilename) || (config.mkdirp 
 		? writeDynamicFilenameCreateDirectories 
 		: writeDynamicFilenameNoDirectoryCreation
 	);
 
 	return write;
 
-	function writeStaticFilename (message) {
-		return fs.appendFileAsync(staticFilename, message.text + EOL);
-	}
-
 	function writeDynamicFilenameCreateDirectories (message) {
 		var filename = dynamicFilename(message);
 		var dirname = path.dirname(filename);
 		return mkdirpAsync(dirname).then(function () {
-			return fs.appendFileAsync(filename, message.text + EOL);
+			return append(filename, message);
 		});
 	}
 
 	function writeDynamicFilenameNoDirectoryCreation (message) {
-		return fs.appendFileAsync(dynamicFilename(message), message.text + EOL);
+		return append(dynamicFilename(message), message);
+	}
+
+	function append (filename, message) {
+		return fs.appendFileAsync(filename, message.text + EOL, config.encoding);
 	}
 }
