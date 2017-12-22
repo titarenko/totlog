954b91d717fab6874c44c5e5450c37437fd6793a
Constantin Titarenko <constantin.titarenko@gmail.com>
2017-12-22 12:56:57 +0200

    add stream

---
+++ stream.js
@@ -0,0 +1,26 @@
+const { Writable } = require('stream')
+const totlog = require('.')
+
+class TotlogStream extends Writable {
+	constructor (options) {
+		super(options)
+		const {
+			level = 'debug',
+			category = __filename,
+		} = options || { }
+		this.level = level
+		this.log = totlog(category)
+	}
+	_write (chunk, encoding, done) {
+		if (typeof chunk !== 'string' && encoding === 'buffer') {
+			chunk = chunk.toString(this._writableState.defaultEncoding)
+		}
+		this.log[this.level](typeof chunk === 'string'
+			? chunk
+			: chunk.toString(encoding)
+		)
+		done()
+	}
+}
+
+module.exports = TotlogStream
---
+++ tests/stream.js
@@ -0,0 +1,23 @@
+const should_ = require('should')
+const mockRequire = require('mock-require')
+const sinon = require('sinon')
+
+describe('stream', function () {
+	let Stream, log, builder
+	beforeEach(function () {
+		log = { debug: sinon.spy() }
+		builder = sinon.spy(() => log)
+		mockRequire('..', builder)
+		Stream = mockRequire.reRequire('../stream')
+	})
+	afterEach(function () {
+		mockRequire.stopAll()
+	})
+	it('should use proper level and category', function () {
+		const stream = new Stream({ level: 'debug', category: 'lol' })
+		stream.write('blah')
+		stream.end()
+		builder.getCall(0).args[0].should.be.eql('lol')
+		log.debug.getCall(0).args[0].should.be.eql('blah')
+	})
+})
