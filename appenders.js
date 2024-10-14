a313d59c1f01d1a787feef244a8560668dca797c
Evgeniy Goryuchkin <goryuchkin@gmail.com>
2024-10-14 21:18:52 +0300

    linter auto fix

--- appenders.js
+++ appenders.js
@@ -5,120 +5,120 @@ const net = require('net')
 
 const log = require('./')(__filename, true)
 
-module.exports =  { slack, logstash }
+module.exports = { slack, logstash }
 
 function slack ({ token, channel, icon }) {
-	if (!token) {
-		throw new Error('Token is required.')
-	}
+  if (!token) {
+    throw new Error('Token is required.')
+  }
 
-	if (!channel) {
-		throw new Error('Channel is required.')
-	}
+  if (!channel) {
+    throw new Error('Channel is required.')
+  }
 
-	return function (ev) {
-		const payload = querystring.stringify({
-			token: token,
-			channel: channel,
-			icon_emoji: icon,
-			mrkdwn: true,
-			text: `*${ev.time}* ${'`'}${ev.category}${'`'} ${'```\n'}${ev.message}${'\n```'}`,
-		})
+  return function (ev) {
+    const payload = querystring.stringify({
+      token: token,
+      channel: channel,
+      icon_emoji: icon,
+      mrkdwn: true,
+      text: `*${ev.time}* ${'`'}${ev.category}${'`'} ${'```\n'}${ev.message}${'\n```'}`,
+    })
 
-		const request = https.request({
-			hostname: 'slack.com',
-			path: '/api/chat.postMessage',
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded',
-				'Content-Length': Buffer.byteLength(payload),
-			},
-		}, handleResponse)
+    const request = https.request({
+      hostname: 'slack.com',
+      path: '/api/chat.postMessage',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': Buffer.byteLength(payload),
+      },
+    }, handleResponse)
 
-		request.on('error', handlError)
-		request.write(payload)
-		request.end()
+    request.on('error', handlError)
+    request.write(payload)
+    request.end()
 
-		function handleResponse (response) {
-			if (response.statusCode != 200) {
-				handlError(new Error(String(response.statusCode)))
-			}
-			response.on('error', handlError)
-		}
+    function handleResponse (response) {
+      if (response.statusCode != 200) {
+        handlError(new Error(String(response.statusCode)))
+      }
+      response.on('error', handlError)
+    }
 
-		function handlError (error) {
-			log.error(`failed to send message to slack due to ${error.stack}`)
-		}
-	}
+    function handlError (error) {
+      log.error(`failed to send message to slack due to ${error.stack}`)
+    }
+  }
 }
 
 function logstash ({ url }) {
-	if (!url) {
-		throw new Error('URL is required.')
-	}
+  if (!url) {
+    throw new Error('URL is required.')
+  }
 
-	const protocol = url.slice(0, 6)
+  const protocol = url.slice(0, 6)
 
-	if (protocol != 'udp://' && protocol != 'tcp://') {
-		throw new Error('Only TCP and UDP protocols are supported.')
-	}
+  if (protocol != 'udp://' && protocol != 'tcp://') {
+    throw new Error('Only TCP and UDP protocols are supported.')
+  }
 
-	url = url.slice(6)
+  url = url.slice(6)
 
-	const [host, port] = url.split(':')
+  const [host, port] = url.split(':')
 
-	if (!port) {
-		throw new Error('Port is required.')
-	}
+  if (!port) {
+    throw new Error('Port is required.')
+  }
 
-	let udpSocket
-	function udp (ev) {
-		if (!udpSocket) {
-			udpSocket = dgram.createSocket('udp4')
-			udpSocket.on('error', error => {
-				log.error(`udp socket error ${error.stack}`)
-				udpSocket.close()
-				udpSocket = null
-			})
-		}
+  let udpSocket
+  function udp (ev) {
+    if (!udpSocket) {
+      udpSocket = dgram.createSocket('udp4')
+      udpSocket.on('error', error => {
+        log.error(`udp socket error ${error.stack}`)
+        udpSocket.close()
+        udpSocket = null
+      })
+    }
 
-		const buffer = new Buffer(JSON.stringify(ev))
-		udpSocket.send(buffer, 0, buffer.length, port, host, error => {
-			if (!error) {
-				return
-			}
-			log.error(`failed to send message to logstash due to ${error.stack}`)
-		})
-	}
+    const buffer = new Buffer(JSON.stringify(ev))
+    udpSocket.send(buffer, 0, buffer.length, port, host, error => {
+      if (!error) {
+        return
+      }
+      log.error(`failed to send message to logstash due to ${error.stack}`)
+    })
+  }
 
-	let tcpSocket
-	function tcp (ev) {
-		if (!tcpSocket) {
-			tcpSocket = net.createConnection({ host, port })
-			tcpSocket.on('error', error => {
-				log.error(`tcp socket error ${error.stack}`)
-				tcpSocket.destroy()
-				tcpSocket = null
-			})
-		}
+  let tcpSocket
+  function tcp (ev) {
+    if (!tcpSocket) {
+      tcpSocket = net.createConnection({ host, port })
+      tcpSocket.on('error', error => {
+        log.error(`tcp socket error ${error.stack}`)
+        tcpSocket.destroy()
+        tcpSocket = null
+      })
+    }
 
-		const buffer = new Buffer(JSON.stringify(ev) + '\n')
-		tcpSocket.write(buffer, error => {
-			if (!error) {
-				return
-			}
-			log.error(`failed to send message to logstash due to ${error.stack}`)
-		})
-	}
+    const buffer = new Buffer(JSON.stringify(ev) + '\n')
+    tcpSocket.write(buffer, error => {
+      if (!error) {
+        return
+      }
+      log.error(`failed to send message to logstash due to ${error.stack}`)
+    })
+  }
 
-	function shutdownTcp () {
-		if (tcpSocket) {
-			tcpSocket.destroy()
-		}
-	}
+  function shutdownTcp () {
+    if (tcpSocket) {
+      tcpSocket.destroy()
+    }
+  }
 
-	process.on('SIGINT', shutdownTcp)
-	process.on('SIGTERM', shutdownTcp)
+  process.on('SIGINT', shutdownTcp)
+  process.on('SIGTERM', shutdownTcp)
 
-	return protocol === 'tcp://' ? tcp : udp
+  return protocol === 'tcp://' ? tcp : udp
 }
--- tests/index.js
+++ tests/index.js
@@ -6,37 +6,37 @@ const sinon = require('sinon')
 require('colors')
 
 describe('log', function () {
-	let time, clock, consoleError, consoleLog
-	beforeEach(function () {
-		time = new Date()
-		clock = sinon.useFakeTimers(time.getTime())
-		consoleError = sinon.stub(console, 'error') // eslint-disable-line no-console
-		consoleLog = sinon.stub(console, 'log') // eslint-disable-line no-console
-	})
-	afterEach(function () {
-		clock.restore()
-		console.error.restore() // eslint-disable-line no-console
-		console.log.restore() // eslint-disable-line no-console
-	})
-	describe('error', function () {
-		it('should use util.format for arg processing', function () {
-			log(__filename).error('obj %j', { a: 1 })
-			const message = `${time.toISOString().white} ${'error'.red} ${'totlog/tests/index.js'.cyan} obj {"a":1}`
-			consoleError.should.be.calledWith(message)
-		})
-	})
-	describe('warn', function () {
-		it('should use util.format for arg processing', function () {
-			log(__filename).warn('obj %j', { a: 2 })
-			const message = `${time.toISOString().white} ${'warn'.yellow} ${'totlog/tests/index.js'.cyan} obj {"a":2}`
-			consoleLog.should.be.calledWith(message)
-		})
-	})
-	describe('debug', function () {
-		it('should use util.format for arg processing', function () {
-			log(__filename).debug('obj %j', { a: 3 })
-			const message = `${time.toISOString().white} debug ${'totlog/tests/index.js'.cyan} obj {"a":3}`
-			consoleLog.should.be.calledWith(message)
-		})
-	})
+  let time, clock, consoleError, consoleLog
+  beforeEach(function () {
+    time = new Date()
+    clock = sinon.useFakeTimers(time.getTime())
+    consoleError = sinon.stub(console, 'error')
+    consoleLog = sinon.stub(console, 'log')
+  })
+  afterEach(function () {
+    clock.restore()
+    console.error.restore()
+    console.log.restore()
+  })
+  describe('error', function () {
+    it('should use util.format for arg processing', function () {
+      log(__filename).error('obj %j', { a: 1 })
+      const message = `${time.toISOString().white} ${'error'.red} ${'totlog/tests/index.js'.cyan} obj {"a":1}`
+      consoleError.should.be.calledWith(message)
+    })
+  })
+  describe('warn', function () {
+    it('should use util.format for arg processing', function () {
+      log(__filename).warn('obj %j', { a: 2 })
+      const message = `${time.toISOString().white} ${'warn'.yellow} ${'totlog/tests/index.js'.cyan} obj {"a":2}`
+      consoleLog.should.be.calledWith(message)
+    })
+  })
+  describe('debug', function () {
+    it('should use util.format for arg processing', function () {
+      log(__filename).debug('obj %j', { a: 3 })
+      const message = `${time.toISOString().white} debug ${'totlog/tests/index.js'.cyan} obj {"a":3}`
+      consoleLog.should.be.calledWith(message)
+    })
+  })
 })
